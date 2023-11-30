5f1b3355492b9f8e7777cdfaf9a5ba6dbe1e4223
bittefensterschliessen <theresakleinert@gmail.com>
2023-11-30 21:52:53 +0100

    fill excel für textfelder fertig

--- fill_excel.py
+++ fill_excel.py
@@ -8,27 +8,25 @@ pdf_path = "forms_to_xls/volle_pdfs/"
 workbook = xlsxwriter.Workbook("forms_to_xls/absolventinnenfeier.xlsx")
 worksheet = workbook.add_worksheet()
 
-# Finde Namen der Pdf Dateien raus
+# Finde Namen der Pdf Dateien
 files = [f for f in os.listdir(pdf_path) if os.path.isfile(os.path.join(pdf_path, f))]
 
-# Namen der Textfelder herausfinden
-reader = PdfReader(formular_pdf)
-all_fields = reader.get_fields()    
+# Finde Namen der Textfelder
+reader = PdfReader(formular_pdf)  
 text_fields = reader.get_form_text_fields()
 
-# Spaltennamen im xls anlegen
+# Schreibe Spaltennamen in xls
 for col, zeilenname in enumerate(text_fields):
     worksheet.write(0, col, zeilenname)
 
 # Schreibe Daten zeilenweise in xls
 for row, file in enumerate(files, start=1):
-    # Öffne jede Zeile das nächste Pdf
+
+    # Öffne jede Zeile das nächste Pdf und lies den Inhalt aus
     reader = PdfReader(pdf_path + file)
+    data = reader.get_form_text_fields()
 
-    for col, value in enumerate(text_fields):
-        # Zwischenstand
-        # Lies im pdf aus, was im Feld mit dem Namen "value" steht
-        worksheet.write(row, col, value)
+    for col, value in enumerate(data):
+        worksheet.write(row, col, data[value])
 
-# xls anlegen
 workbook.close()
