fe0d7cb2c0a56d929018caa269de7b092b1bb376
pfaffelh <p.p@stochastik.uni-freiburg.de>
2023-12-06 23:55:25 +0100

    updated fill_pdf.py

--- fill_pdf.py
+++ fill_pdf.py
@@ -1,54 +1,113 @@
-from pypdf import PdfReader, PdfWriter
+import argparse, sys, os
 import pandas as pd
-import sys
-import os
+from pathlib import Path
+from pypdf import PdfReader, PdfWriter
 
-def write_pdfs(pdf_form, excel_file, pdf_path, name_by):
-
-    # Lies leeres Formular
-    reader = PdfReader(pdf_form)
+# Fill in field names in an empty pdf form
+def write_pdf_form_with_field_names(filename_pdf_empty, filename_pdf_filled):
+    reader = PdfReader(filename_pdf_empty)
+    writer = PdfWriter()
     text_fields = reader.get_form_text_fields()
 
-    # Lies ausgefülltes xls in einen dataframe ein
-    df = pd.read_excel(excel_file)
-    df.fillna("", inplace=True)  # fülle missing values mit leerem String
+    writer.append(reader)
+    for i in range(len(reader.pages)):
+        for key in text_fields:
+            writer.update_page_form_field_values(
+                writer.pages[i], {key: key}
+            )
+    with open(filename_pdf_filled, "wb") as output_stream:
+        writer.write(output_stream)
+    print("File " + filename_pdf_filled + " written." )
 
+# Check if a field in a pdf form is a Checkbox
+def is_check_box(value):
+    return "/_States_" in value
+
+# Fill in pdf form with data from an excel file
+# output will be stored in pdf_path, and named by name_by-column of the excel file
+def write_pdfs(pdf_form, excel_file, pdf_path, name_by = None):
+   # Read xls in a data frame
+    path_to_excel = Path(excel_file)
+    if not path_to_excel.exists():
+        print(excel_file + " does not exist. Exiting...")
+        sys.exit()
+    df = pd.read_excel(excel_file)
+    # fill missing values by an empty string
+    df.fillna("", inplace=True)  
+
+    # Read empty form
+    if not Path(pdf_form).exists():
+        print(pdf_form + " does not exist. Exiting...")
+        sys.exit()
+    reader = PdfReader(pdf_form)
+    fields = reader.get_fields()    
+
+    # Check Output-Pfad
+    if not Path(pdf_path).is_dir():
+        print(pdf_path + " is not a directory. Exiting...")
+        sys.exit()
+    print("Write pdfs in " + pdf_path)
+
+    # Write one file for each row in the excel file.
+    # Use column name_by for filenames.
+    if (not name_by == None) and (not name_by in df.columns):
+        print(name_by + " is not a column name in " + excel_file + ". Exiting...")
+        sys.exit()
     for index, row in df.iterrows():
         writer = PdfWriter()
         writer.append(reader)
         for i in range(len(reader.pages)):
-            for field in text_fields:
-                try:
-                    writer.update_page_form_field_values(writer.pages[i], {field: row[field]})
-                except:
-                    print("Problems with field " + field)
-            # set_need_appearances_writer
-            # write "output" to pypdf-output.pdf
-        filename = row[name_by] + ".pdf"
-        with open(os.path.join(pdf_path, filename), "wb") as output_stream:
+            for key, field in fields.items():
+                if is_check_box(fields[key]):
+                    states = field["/_States_"]
+                    if not str(row[key]).lower() in {"", "nein", "off", "false", "0"}:
+                        state = [s for s in states if s != "/Off"][0]
+                        writer.update_page_form_field_values(writer.pages[i], {key: state}, auto_regenerate=False)
+                else:
+                    try:
+                        writer.update_page_form_field_values(writer.pages[i], {key: row[key]})
+                    except:
+                        print("Probleme mit Feld " + key)
+
+        if name_by == None:
+            filename = (args.output + "/" + str(index + 1) + ".pdf")
+        else:
+            filename = (args.output + "/" + row[name_by] + ".pdf")
+        print(filename)
+        with open(filename, "wb") as output_stream:
             writer.write(output_stream)
-        print("Datei " + filename + " geschrieben.")
+
 
 if __name__ == "__main__":
 
-    # pdf_form : Pfad zum Formular, das ausgefüllt werden soll
-    # excel_filename: Pfad zur ausgefüllten Excel Datei
-    # path_to_folder: Pfad zum Ordner, in dem die ausgefüllten Pdfs gespeichert werden sollen
-    # name_by: Feld des Formulars, nach dem die Pdf Dateien benannt werden sollen
+    # pdf_form : path to form which needs to be filled
+    # excel_filename: path to excel file for data
+    # path_to_folder: path to output folder for filled pdfs
+    # name_by: None, or the field of the form for filenames
 
-    instructions = f"""
-    Benutzung: python {sys.argv[0]} <pdf_form> <excel_filename> <path_to_folder> <name_by>
-    """
+    # Get arguments from command line, creating the help at the same time.
+    parser = argparse.ArgumentParser(description='Fill in pdf forms from an xls-file.')
+    parser.add_argument('pdf_form',
+                     help='The empty pdf form.')
+    parser.add_argument('-f', '--excel_filename', default = None,
+                     help='The Excel file for the data used to fill out the forms. Column names must match keys for the fields.  If no xls-file is given, only field names are filled into the form.')
+    parser.add_argument('-o', '--output', default = "output",
+                     help='The output folder for the filled pdfs.')
+    parser.add_argument('-s', '--name_by', default = None,
+                     help='Filled pdf files will be named after field name_by.')
+    args = parser.parse_args()
 
-    # Shell Argumente parsen
-    if len(sys.argv) < 5:
-        print(instructions)
-        sys.exit()
+    # check output path
+    path_to_output = Path(args.output)
+    if args.output == "output":
+        print("Will create folder output/ in current folder.")
+        if path_to_output.exists():
+            print("output/ already exists. Exiting...")
+            sys.exit()
+        os.mkdir(args.output)
 
-    pdf_form = sys.argv[1]
-    excel_file = sys.argv[2]  # hier liegts
-    pdf_path = sys.argv[3]  # hier solls rein
-    name_by = sys.argv[4]
+    if args.excel_filename == None:
+        write_pdf_form_with_field_names(args.pdf_form, args.output + "/" + "filled_form.pdf")
+    else:
+        write_pdfs(args.pdf_form, args.excel_filename, args.output, args.name_by)
 
-    # Schreibe pdfs
-    write_pdfs(pdf_form, excel_file, pdf_path, name_by)
